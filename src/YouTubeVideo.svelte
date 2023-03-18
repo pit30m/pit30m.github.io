ff2e551256ddd73ddab31674cff96ef8d6bf1626
una-dinosauria <jltmtzc@gmail.com>
2023-03-18 17:53:55 -0400

    add tailwind, first version of website

---
+++ src/YouTubeVideo.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let videoId;
+  export let width = "560";
+  export let height = "315";
+</script>
+
+<div class="w-full max-w-xl mx-auto my-8">
+  <div class="relative h-0" style="padding-bottom: 56.25%">
+    <iframe
+      title="YouTube video player"
+      class="absolute top-0 left-0 w-full h-full"
+      src={`https://www.youtube.com/embed/${videoId}`}
+      width={width}
+      height={height}
+      frameborder="0"
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>
+  </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,24 @@
 <script>
-    let name = "meow"
+    import YouTubeVideo from '../YouTubeVideo.svelte';
+    let datasetName = "Pit30M"
 </script>
 
-<h1>Welcome to SvelteKit {name}</h1>
+<main class="bg-gray-200 min-h-screen flex items-center justify-center">
+<div class="text-center">
+    <h1 class="text-5xl font-bold mb-6">Welcome to {datasetName}</h1>
+    <p class="text-xl mb-8">Thanks for visiting!</p>
+    <a href="#" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Get started</a>
+    <YouTubeVideo videoId="hJ6A_1YSITo" />
+</div>
+
+
+
+</main>
+
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+</style>

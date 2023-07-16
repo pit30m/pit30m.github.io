649269b23e983362e6fe60cdc91f35202a70afc7
una-dinosauria <jltmtzc@gmail.com>
2023-07-16 00:45:21 -0400

    try to deploy

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,7 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-            fallback: '200.html' // may differ from host to host
+            fallback: 'index.html' // may differ from host to host
         })
 	},
 
